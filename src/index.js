27577d2f5cd7abb85a8949447d821bcb43339d52
han <devhhg1993@gmail.com>
2022-11-03 12:59:03 +0900

    frontend folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Login";
+import Main from "./Main";
+import Join from "./Join";
+import PostEdit from "./PostEdit";
+import PostInfo from "./PostInfo";
+import Header from "./Header";
+import PostUpdate from "./PostUpdate";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Header/>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/post" element={<PostEdit/>}/>
+              <Route path="/post/:id" element={<PostInfo/>}/>
+              <Route path="/post/update/:id" element={<PostUpdate/>}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/join" element={<Join />}/>
+              <Route path="/" element={<Main />}/>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
